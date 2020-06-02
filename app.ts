8c43db163915e8f293363c8105eb7d1582cd4044
loterio <vitorloterio@gmail.com>
2020-06-02 18:34:41 -0300

    fix the output msgs

--- app.ts
+++ app.ts
@@ -2,10 +2,17 @@ let message: string = "and I am iron man.";
 console.log(message);
 
 let nbmark: number = 32;
-console.log("this is the strongest mark " + nbmark);
+console.log("the strongest is mark " + nbmark);
 nbmark+=7;
-console.log("but the best one is " + nbmark);
+console.log("but the best one is mark " + nbmark);
+bestArmour("Fabio");
 
-function useTheForce(name:string):void{
-    console.log("Use the force"+name);
+function bestArmour(name:string):void{ // void significa que a função não terá retorno
+    console.log("mark 42 is the beatifuly, " + name);
 }
+
+let menorRun = shortestRun(9);
+function shortestRun(parsecs:number):boolean{
+    return parsecs < 12;
+}
+console.log(menorRun);
