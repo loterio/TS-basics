f5bbbae2f3c75a7046d846c97761a9c64a143293
loterio <vitorloterio@gmail.com>
2020-06-02 18:07:36 -0300

    fix name of file

---
+++ syntax.ts
@@ -0,0 +1,19 @@
+// Neste arquivo constará as estruturas básicas da sintaxe TypeScript
+
+// Variáveis
+
+let nm: string      // ao declararmos uma variável com 'let', podemos ja definir sua tipagem
+let num: number     // caso um tipo seja escolhido, a variável só podera assumir valores 
+let stt: boolean    // compatíveis com o tipo escolhido, gerando um erro quando não o feito.
+
+// ou, caso não queira especificar 
+// o tipo já, é possivel:
+
+let test            // o tipo de variável será 
+let test2: any      // definido quando ela for usada
+
+// Arrays
+
+let players: number[] = [1,2,3]
+let players2: Array<number>=[1,2,3]     // assume o tipo entre as chaves '< >'
+let players3: [1,2,3]
